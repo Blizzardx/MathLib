fb7fe7686ae21cb8599ae8daabc862607f6a1437
baoxue <Blizzard_xc@126.com>
2016-05-17 10:11:05 +0800

    init math client

---
+++ MathLib/MathLib.cpp
@@ -0,0 +1,28 @@
+// MathLib.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Vector3.h"
+#include <iostream>
+using namespace MathF;
+
+int main()
+{
+	Vector3 a = Vector3::One;
+	Vector3 b = Vector3::Zero;
+	b = a-b;
+	a = a / 10;
+	Vector3 c = a *= 20;
+	c.x = 12;
+	std::cout << a.x << std::endl;
+	std::cout << a.y << std::endl;
+	std::cout << a.z << std::endl;
+	std::cout << b.x << std::endl;
+	std::cout << b.y << std::endl;
+	std::cout << b.z << std::endl;
+	std::cout << c.x << std::endl;
+
+	system("pause");
+    return 0;
+}
+
